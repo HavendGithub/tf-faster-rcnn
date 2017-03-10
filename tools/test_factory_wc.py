61c92fe7f896c12950d6d1c4611cb1a606a115f0
WENXI CHEN <wenxichen@WENXIs-MacBook-Pro.local>
2017-03-09 22:19:48 -0500

    minor fix

--- tools/test_factory_wc.py
+++ tools/test_factory_wc.py
@@ -3,6 +3,7 @@ from datasets.factory import get_imdb
 
 if __name__ == "__main__":
 
+  print "it's here"
   imdb = get_imdb('voc_2007_trainval')
   print('Loaded dataset `{:s}` for training'.format(imdb.name))
 
